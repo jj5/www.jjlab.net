08a5d03bc6042e0c1ae488ed1e99444c764c5d25
John Elliot V <jj5@jj5.net>
2024-07-06 18:55:36 +1000

    Work, work...

--- bin/admin/deploy.sh
+++ bin/admin/deploy.sh
@@ -8,26 +8,8 @@ main() {
 
   cd "../../";
 
-  lx_run deploy grace /var/www/bk-web-1-www.jjlab.net;
+  lx_run deploy_all;
 
 }
 
-deploy() {
-
-  local host="${1}";
-  local path="${2}";
-
-  lx_ssh "$host" /srv/libexec/bin/lx-update-web.sh "$path";
-
-  lx_note "deployed to $host:$path";
-
-  lx_ssh lore sudo /srv/admin/bin/lore-archive-import.sh "$host";
-
-  lx_note "deployed to $host:$path then imported archives from '$host'.";
-
-}
-
-# 2024-07-06 jj5 - NEW: just one little indirection so I can use a different libexec on 'charisma'
-source "$( dirname "${BASH_SOURCE[0]}" )/../../inc/run.sh";
-# 2024-07-06 jj5 - OLD:
-#source "$( dirname "${BASH_SOURCE[0]}" )/../../ext/libexec/inc/run.sh";
+source "$( dirname "${BASH_SOURCE[0]}" )/inc/admin.sh";
---
+++ bin/admin/inc/admin.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+deploy_all() {
+
+  lx_run deploy grace /var/www/bk-web-1-www.jjlab.net;
+
+}
+
+deploy() {
+
+  local host="${1}";
+  local path="${2}";
+
+  lx_ssh "$host" /srv/libexec/bin/lx-update-web.sh "$path";
+
+  lx_note "deployed to $host:$path";
+
+  lx_ssh lore sudo /srv/admin/bin/lore-archive-import.sh "$host";
+
+  lx_note "deployed to $host:$path then imported archives from '$host'.";
+
+}
+
+source "$( dirname "${BASH_SOURCE[0]}" )/../../../inc/run.sh";
