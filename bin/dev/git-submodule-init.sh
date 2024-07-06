5b9fcfd9404ffd9874155a0a9d5258a7c52d78a5
John Elliot V <jj5@jj5.net>
2024-07-06 19:31:23 +1000

    Work, work...

--- bin/dev/git-submodule-init.sh
+++ bin/dev/git-submodule-init.sh
@@ -13,6 +13,28 @@ main() {
 
   git submodule update --init --recursive
 
+  pushd ext/libexec;
+
+    git checkout main;
+
+    git config -f .gitmodules submodule.submodule-path.branch main;
+
+  popd;
+
+  pushd ext/mudball;
+
+    git checkout main;
+
+    git config -f .gitmodules submodule.submodule-path.branch main;
+
+  popd;
+
+  git add .gitmodules ext;
+
+  git commit -m 'Work, work...';
+
+  git push;
+
 }
 
 main "$@";
