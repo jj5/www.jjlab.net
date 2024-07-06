811ed18215628c8678ab261037d03c6bb14ba7dd
John Elliot V <jj5@jj5.net>
2024-07-07 08:43:55 +1000

    Work, work...

--- inc/version.php
+++ inc/version.php
@@ -2,11 +2,11 @@
 // these version numbers are managed by bin/dev/version.sh...
 define( 'JJLAB_VERSION_MAJOR', 0 );
 define( 'JJLAB_VERSION_MINOR', 2 );
-define( 'JJLAB_VERSION_PATCH', 543 );
+define( 'JJLAB_VERSION_PATCH', 545 );
 define(
   'JJLAB_SVN_DATE',
-  '$Date: 2024-07-07 08:40:27 +1000 (Sun, 07 Jul 2024) $'
+  '$Date: 2024-07-07 08:43:38 +1000 (Sun, 07 Jul 2024) $'
 );
-define( 'JJLAB_SVN_REVISION', '$Revision: 160567432 $' );
+define( 'JJLAB_SVN_REVISION', '$Revision: 225137063 $' );
 define( 'JJLAB_SVN_AUTHOR', '$Author: jj5 $' );
-define( 'JJLAB_GIT_DATE', '2024-07-07 08:40:27 +1000 (Sun, 07 Jul 2024)' );
+define( 'JJLAB_GIT_DATE', '2024-07-07 08:43:38 +1000 (Sun, 07 Jul 2024)' );
