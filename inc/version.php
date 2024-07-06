b59ac160fb5ffbebbe41517b7a5403beec064e51
John Elliot V <jj5@jj5.net>
2024-07-06 21:47:21 +1000

    Work, work...

--- inc/version.php
+++ inc/version.php
@@ -2,11 +2,11 @@
 // these version numbers are managed by bin/dev/version.sh...
 define( 'JJLAB_VERSION_MAJOR', 0 );
 define( 'JJLAB_VERSION_MINOR', 2 );
-define( 'JJLAB_VERSION_PATCH', 523 );
+define( 'JJLAB_VERSION_PATCH', 525 );
 define(
   'JJLAB_SVN_DATE',
-  '$Date: 2024-07-06 21:43:10 +1000 (Sat, 06 Jul 2024) $'
+  '$Date: 2024-07-06 21:47:05 +1000 (Sat, 06 Jul 2024) $'
 );
-define( 'JJLAB_SVN_REVISION', '$Revision: 34285280 $' );
+define( 'JJLAB_SVN_REVISION', '$Revision: 90315288 $' );
 define( 'JJLAB_SVN_AUTHOR', '$Author: jj5 $' );
-define( 'JJLAB_GIT_DATE', '2024-07-06 21:43:10 +1000 (Sat, 06 Jul 2024)' );
+define( 'JJLAB_GIT_DATE', '2024-07-06 21:47:05 +1000 (Sat, 06 Jul 2024)' );
