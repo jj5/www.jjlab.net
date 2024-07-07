73306418702a5804b42dab0941ccf374c64f62d8
John Elliot V <jj5@jj5.net>
2024-07-07 12:24:42 +1000

    Work, work...

--- src/code/1-bootstrap/2-constant.php
+++ src/code/1-bootstrap/2-constant.php
@@ -11,11 +11,11 @@ require_once __DIR__ . '/../../../inc/version.php';
 
 // 2024-07-06 jj5 - changing the code is not supported, please don't change it...
 //
-define( 'JJLAB_CODE', 'jjlab' );
+//define( 'JJLAB_CODE', 'jjlab' );
 
 // 2024-07-06 jj5 - you can change the name if you want...
 //
-define( 'JJLAB_NAME', 'www.jjlab.net' );
+//define( 'JJLAB_NAME', 'www.jjlab.net' );
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
