08cc3eac2b390d5517f4278611c0d2e66acc02c6
John Elliot V <jj5@jj5.net>
2024-07-30 13:47:35 +1000

    Work, work...

--- src/code/5-module/510-app/3-model/AppStash.php
+++ src/code/5-module/510-app/3-model/AppStash.php
@@ -331,7 +331,7 @@ class AppStash {
 
   public function get_html_id( $string ) {
 
-    $string = preg_replace( '/[^a-z0-9.-]+/i', '-', $string );
+    $string = preg_replace( '/[^❤️a-z0-9.-]+/i', '-', $string );
 
     $string = preg_replace( '/-+/', '-', $string );
 
