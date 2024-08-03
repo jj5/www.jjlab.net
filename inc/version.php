7c327a0d5c654b48c876841e8f467fcc75081a1b
John Elliot V <jj5@jj5.net>
2024-08-03 10:00:20 +1000

    Work, work...

--- inc/version.php
+++ inc/version.php
@@ -6,16 +6,16 @@
 define( 'JJLAB_NAME', 'www.jjlab.net' );
 define( 'JJLAB_CODE', 'jjlab' );
 
-//define( 'JJLAB_VERSION', '0.2.935' );
+//define( 'JJLAB_VERSION', '0.2.937' );
 define( 'JJLAB_VERSION_MAJOR', 0 );
 define( 'JJLAB_VERSION_MINOR', 2 );
-define( 'JJLAB_VERSION_PATCH', 935 );
+define( 'JJLAB_VERSION_PATCH', 937 );
 
 define(
   'JJLAB_SVN_DATE',
-  '$Date: 2024-08-03 04:55:35 +1000 (Sat, 03 Aug 2024) $'
+  '$Date: 2024-08-03 10:00:20 +1000 (Sat, 03 Aug 2024) $'
 );
-define( 'JJLAB_SVN_REVISION', '$Revision: 310670 $' );
+define( 'JJLAB_SVN_REVISION', '$Revision: 215411247 $' );
 define( 'JJLAB_SVN_AUTHOR', '$Author: jj5 $' );
 
-define( 'JJLAB_GIT_DATE', '2024-08-03 04:55:35 +1000 (Sat, 03 Aug 2024)' );
+define( 'JJLAB_GIT_DATE', '2024-08-03 10:00:20 +1000 (Sat, 03 Aug 2024)' );
--- inc/version.sh
+++ inc/version.sh
@@ -5,13 +5,13 @@
 export JJLAB_NAME=''www.jjlab.net'';
 export JJLAB_CODE=''jjlab'';
 
-export JJLAB_VERSION='0.2.935';
+export JJLAB_VERSION='0.2.937';
 export JJLAB_VERSION_MAJOR='0';
 export JJLAB_VERSION_MINOR='2';
-export JJLAB_VERSION_PATCH='935';
+export JJLAB_VERSION_PATCH='937';
 
-export JJLAB_SVN_DATE='Date: 2024-08-03 04:55:35 +1000 (Sat, 03 Aug 2024)';
-export JJLAB_SVN_REVISION='Revision: 310670';
+export JJLAB_SVN_DATE='Date: 2024-08-03 10:00:20 +1000 (Sat, 03 Aug 2024)';
+export JJLAB_SVN_REVISION='Revision: 215411247';
 export JJLAB_SVN_AUTHOR='Author: jj5';
 
-export JJLAB_GIT_DATE='2024-08-03 04:55:35 +1000 (Sat, 03 Aug 2024)';
+export JJLAB_GIT_DATE='2024-08-03 10:00:20 +1000 (Sat, 03 Aug 2024)';
