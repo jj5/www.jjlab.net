da70e1c4a52e976f70a92d41f7d0725c5e7534c3
John Elliot V <john@blackbrick.com.au>
2024-08-14 16:06:58 +1000

    Work, work...

--- inc/version.sh
+++ inc/version.sh
@@ -5,13 +5,13 @@
 export JJLAB_NAME=''www.jjlab.net'';
 export JJLAB_CODE=''jjlab'';
 
-export JJLAB_VERSION='0.3.1293';
+export JJLAB_VERSION='0.3.1295';
 export JJLAB_VERSION_MAJOR='0';
 export JJLAB_VERSION_MINOR='3';
-export JJLAB_VERSION_PATCH='1293';
+export JJLAB_VERSION_PATCH='1295';
 
-export JJLAB_SVN_DATE='Date: 2024-08-14 15:51:11 +1000 (Wed, 14 Aug 2024)';
-export JJLAB_SVN_REVISION='Revision: 12668276251';
+export JJLAB_SVN_DATE='Date: 2024-08-14 16:06:57 +1000 (Wed, 14 Aug 2024)';
+export JJLAB_SVN_REVISION='Revision: 5639089847';
 export JJLAB_SVN_AUTHOR='Author: jj5';
 
-export JJLAB_GIT_DATE='2024-08-14 15:51:11 +1000 (Wed, 14 Aug 2024)';
+export JJLAB_GIT_DATE='2024-08-14 16:06:57 +1000 (Wed, 14 Aug 2024)';
