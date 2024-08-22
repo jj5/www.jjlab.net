40d46e8c4bf58131b60ce1d2f838f488623eb3be
John Elliot V <jj5@jj5.net>
2024-08-23 07:26:37 +1000

    Work, work...

--- inc/version.sh
+++ inc/version.sh
@@ -5,13 +5,13 @@
 export JJLAB_NAME=''www.jjlab.net'';
 export JJLAB_CODE=''jjlab'';
 
-export JJLAB_VERSION='0.3.1331';
+export JJLAB_VERSION='0.3.1333';
 export JJLAB_VERSION_MAJOR='0';
 export JJLAB_VERSION_MINOR='3';
-export JJLAB_VERSION_PATCH='1331';
+export JJLAB_VERSION_PATCH='1333';
 
-export JJLAB_SVN_DATE='Date: 2024-08-23 07:20:46 +1000 (Fri, 23 Aug 2024)';
-export JJLAB_SVN_REVISION='Revision: 40749580929';
+export JJLAB_SVN_DATE='Date: 2024-08-23 07:26:37 +1000 (Fri, 23 Aug 2024)';
+export JJLAB_SVN_REVISION='Revision: 36747284696';
 export JJLAB_SVN_AUTHOR='Author: jj5';
 
-export JJLAB_GIT_DATE='2024-08-23 07:20:46 +1000 (Fri, 23 Aug 2024)';
+export JJLAB_GIT_DATE='2024-08-23 07:26:37 +1000 (Fri, 23 Aug 2024)';
