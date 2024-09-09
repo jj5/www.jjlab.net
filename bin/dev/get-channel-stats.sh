a6a11570a013882cde9e0b1bc2b4f4ab93401151
John Elliot V <jj5@jj5.net>
2024-09-09 18:37:20 +1000

    Work, work...

--- bin/dev/get-channel-stats.sh
+++ bin/dev/get-channel-stats.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+CHANNEL_LIST=(
+  EEVblog
+);
+
+STATS_DIR='src/web/root/in-the-lab/stats';
+
 if [ -z "${LX_LOCK_FILE:-}" ]; then
 
   LX_LOCK_FILE='/var/lock/jjlab.net-stats.lock';
@@ -12,11 +18,9 @@ main() {
 
   cd "$( dirname "${BASH_SOURCE[0]}" )/../../";
 
-  local list=(
-    EEVblog
-  );
+  [ -d "$STATS_DIR" ] || { lx_fail $LX_EXIT_FILE_MISSING "directory missing: $STATS_DIR"; };
 
-  for channel in "${list[@]}"; do
+  for channel in "${CHANNEL_LIST[@]}"; do
 
     local slug="@$channel";
 
@@ -28,19 +32,13 @@ main() {
 
   lx_run bin/dev/push.sh;
 
-  for channel in "${list[@]}"; do
+  for channel in "${CHANNEL_LIST[@]}"; do
 
     local slug="@$channel";
 
     lx_note "rendering stats for $slug";
 
-    local html_file="src/web/root/in-the-lab/stats/$channel.html";
-
-    [ -f $html_file ] || {
-
-      lx_fail $LX_EXIT_FILE_MISSING "file not found: $html_file";
-
-    };
+    local html_file="$STATS_DIR/$channel.html";
 
     curl https://www.inthelabwithjayjay.com/in-the-lab/stats.php/$slug > $html_file;
 
