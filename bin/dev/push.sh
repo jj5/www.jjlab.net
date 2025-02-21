ebddc50041391381f40c34ada222206b03e6241e
John Elliot V <jj5@jj5.net>
2025-02-22 08:49:47 +1100

    Work, work...

--- bin/dev/push.sh
+++ bin/dev/push.sh
@@ -4,10 +4,6 @@ main() {
 
   jjlab_run_dev jjlab_push;
 
-  # 2025-02-22 jj5 - HACK: this just fixes up the konsole window title...
-  #
-  echo -ne "\033]0;$USER@$HOSTNAME\007"
-
 }
 
 source "$( dirname "${BASH_SOURCE[0]}" )/inc/dev.sh";
