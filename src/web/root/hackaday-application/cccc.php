a45fd14d887c3f2f391d798d602ba4376416d99a
John Elliot V <jj5@jj5.net>
2025-02-26 16:37:35 +1100

    Work, work...

---
+++ src/web/root/hackaday-application/cccc.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/template.php';
+
+render_head( 'Crustacean Chirpy Chip Challenge', 'img/jjlab-2.jpg' );
+
+?>
+
+<p>TODO</p>
+
+<hr>
+
+<?php
+
+render_index();
+
+render_foot();
