a47a89fc55ab95defaf1ae1148331c02eb9057aa
John Elliot V <jj5@jj5.net>
2025-02-27 17:57:25 +1100

    Work, work...

--- src/web/root/hackaday-application/jjlab.php
+++ src/web/root/hackaday-application/jjlab.php
@@ -6,7 +6,50 @@ render_head( 'Introducing In The Lab With Jay Jay', 'img/jjlab-2.jpg' );
 
 ?>
 
-<p>TODO</p>
+<p>There's a new electronics
+<a href="https://hackaday.com/tag/video-blog/">video blog</a>
+in town! In The Lab With Jay Jay is brought to you by John Elliot V, also known as
+your mate, Jay Jay.</p>
+
+<p>Jay Jay has been at it for a while now and is making progress when it comes to making interesting electronics videos.</p>
+
+<p>Inspired by others, such as Dave Jones over on the
+<a href="https://hackaday.com/2009/08/16/eevblog-the-electronics-engineering-video-blog/">EEVblog</a>,
+Jay Jay aims to make a show that is both entertaining and educational. The show is aimed at beginners and intermediate
+electronics enthusiasts and is designed to be a fun and engaging way to learn about electronics.</p>
+
+<p>The channel format is slowly evolving and there are now a number of distinct features of the show which are released
+as standalone segments.</p>
+
+<p>The show's main features include:</p>
+
+<ul>
+<li><strong>Electronics Projects:</strong> wherein we do an ad hoc electronics project. This can be as simple as making a cable or
+as complex as programming a microcontroller.</li>
+<li><strong>Old Book Teardowns:</strong> wherein we take a look at an old electronics book and see what's inside. Old books are anything
+published before the year 2000.</li>
+<li><strong>New Book Teardowns:</strong> wherein we take a look at a new electronics book and see what's inside. New books are anything
+published during or since the year 2000.</li>
+<li><strong>Mini Projects:</strong> wherein we do the
+<a href="https://www.siliconchip.com.au/Series/417">Mini Projects</a>
+published by
+<a href="https://www.siliconchip.com.au/">Silicon Chip</a>
+magazine. These are small projects that
+are published in the magazine and are designed to be built in a day.</li>
+<li><strong>Maxitronix Xin1 Kits:</strong> wherein we build the Maxitronix Xin1 kits. These are kits that are designed to teach electronics
+to children and they are great fun and highly educational.</li>
+<li><strong>Interludes:</strong> wherein we take a break from the main content and do something different.</li>
+<li><strong>Mail Call:</strong> wherein we take a look at what has arrived in the mail. This can be anything from a new book to a new
+component to a new tool, or a few boxes full of such things.</li>
+<li><strong>Unboxings:</strong> wherein we unbox something new. This can be anything from a new book to a new component to a new tool.</li>
+<li><strong>Demos:</strong> wherein we take a close look at something, usually a new tool or a new component, and see how it works.
+When practical we go through the manual and take a look at each feature.</li>
+<li><strong>Channel News:</strong> wherein we take a look at what's happening on the channel.</li>
+</ul>
+
+<p>If any of that sounds of interest to you check us out on the web at
+<a href="https://www.inthelabwithjayjay.com/">InTheLabWithJayJay.com</a> or on YouTube at
+<a href="https://www.youtube.com/@InTheLabWithJayJay">YouTube.com/@InTheLabWithJayJay</a>.</p>
 
 <hr>
 
