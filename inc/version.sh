2c31e9fb0e5f42c3b19869df058e7e3d96f68601
John Elliot V <jj5@jj5.net>
2025-02-27 18:00:49 +1100

    Work, work...

--- inc/version.sh
+++ inc/version.sh
@@ -5,13 +5,13 @@
 export JJLAB_NAME='www.jjlab.net';
 export JJLAB_CODE='jjlab';
 
-export JJLAB_VERSION='0.3.1723';
+export JJLAB_VERSION='0.3.1725';
 export JJLAB_VERSION_MAJOR='0';
 export JJLAB_VERSION_MINOR='3';
-export JJLAB_VERSION_PATCH='1723';
+export JJLAB_VERSION_PATCH='1725';
 
-export JJLAB_SVN_DATE='Date: 2025-02-27 17:57:28 +1100 (Thu, 27 Feb 2025)';
-export JJLAB_SVN_REVISION='Revision: 706430499925';
+export JJLAB_SVN_DATE='Date: 2025-02-27 18:00:49 +1100 (Thu, 27 Feb 2025)';
+export JJLAB_SVN_REVISION='Revision: 408451093549';
 export JJLAB_SVN_AUTHOR='Author: jj5';
 
-export JJLAB_GIT_DATE='2025-02-27 17:57:28 +1100 (Thu, 27 Feb 2025)';
+export JJLAB_GIT_DATE='2025-02-27 18:00:49 +1100 (Thu, 27 Feb 2025)';
