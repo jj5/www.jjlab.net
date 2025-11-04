13cdc68ef56174dae7c1d1eef1f5f65ce1829028
John Elliot V <jj5@jj5.net>
2025-11-04 11:55:03 +1100

    Work, work...

--- src/code/5-module/510-app/spec/spec-show.php
+++ src/code/5-module/510-app/spec/spec-show.php
@@ -3804,6 +3804,22 @@ show(
   ),
 );
 
+show(
+  silly_job_title( 'Current Connoisseur' ),
+  maxitronix_segment(
+    publication_date( '2025-11-06' ),
+    title( 'The "Invisible Power" Radio' ),
+    project( 4, 30 ),
+    maxitronix_kit_name( '30in1' ),
+    youtube_video( 'eFXAw9nwCkE' ),
+    blog_url( 'https://blog.jj5.net/blog/2025/11/06/the-invisible-power-radio-project-4-30-maxitronix-30in1-in-the-lab-with-jay-jay/' ),
+    patreon_url( 'https://www.patreon.com/posts/142779148' ),
+    tags(
+      'jjlab', 'Maxitronix 30in1', 'Electronics Project', 'Lab', 'Kit', 'Circuit', 'Experiment',
+      'The Invisible Power Radio',
+    ),
+  ),
+);
 
 /*
 show(
