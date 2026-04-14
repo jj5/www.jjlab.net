3c1653fc662ac924e39ed12f1e060ab6b32981eb
John Elliot V <jj5@jj5.net>
2026-04-14 20:49:32 +1000

    Work, work...

--- inc/version.php
+++ inc/version.php
@@ -5,16 +5,16 @@
 define( 'JJLAB_NAME', 'jjlab-0.3' );
 define( 'JJLAB_CODE', 'jjlab' );
 
-//define( 'JJLAB_VERSION', '0.3.2175' );
+//define( 'JJLAB_VERSION', '0.3.2177' );
 define( 'JJLAB_VERSION_MAJOR', 0 );
 define( 'JJLAB_VERSION_MINOR', 3 );
-define( 'JJLAB_VERSION_PATCH', 2175 );
+define( 'JJLAB_VERSION_PATCH', 2177 );
 
 define(
   'JJLAB_SVN_DATE',
-  '$Date: 2026-04-14 18:58:13 +1000 (Tue, 14 Apr 2026) $'
+  '$Date: 2026-04-14 20:49:32 +1000 (Tue, 14 Apr 2026) $'
 );
-define( 'JJLAB_SVN_REVISION', '$Revision: 4995221467215 $' );
+define( 'JJLAB_SVN_REVISION', '$Revision: 12216276693181 $' );
 define( 'JJLAB_SVN_AUTHOR', '$Author: jj5 $' );
 
-define( 'JJLAB_GIT_DATE', '2026-04-14 18:58:13 +1000 (Tue, 14 Apr 2026)' );
+define( 'JJLAB_GIT_DATE', '2026-04-14 20:49:32 +1000 (Tue, 14 Apr 2026)' );
