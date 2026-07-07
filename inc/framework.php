6b917014bfd6aa0f3a2a3d9bfcc62df650a64a2f
John Elliot V <jj5@jj5.net>
2026-07-07 20:41:52 +1000

    Work, work...

--- inc/framework.php
+++ inc/framework.php
@@ -2,5 +2,7 @@
 
 define( 'APP_BASE_DIR', realpath( realpath( __DIR__ ) . '/../' ) );
 
+require_once __DIR__ . '/../debug.php';
+
 require_once __DIR__ . '/../src/code/5-module/510-app/inc/framework.php';
 require_once __DIR__ . '/../src/code/1-bootstrap/9-keystone.php';
